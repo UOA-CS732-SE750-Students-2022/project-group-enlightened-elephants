a8e0263a4077aab1d9b52b7504fe23841fbfa2df
ZTianWang <wnwn1994@163.com>
2022-05-13 15:13:47 +0800

    add API for add, update, delete and like post

--- backend/src/data/post-data/eepostDao.js
+++ backend/src/data/post-data/eepostDao.js
@@ -14,7 +14,32 @@ async function retrieveEepostListByEntryId(entry_id,pageNum) {
     return await Eepost.find({entry_id : entry_id}).sort({createdAt:-1}).skip(10*(pageNum-1)).limit(10).populate('comments')
 }
 
+// Add a new post
+async function addEepost(eepost) {
+    const dbEepost = new Eepost(eepost)
+    return await dbEepost.save()
+}
+
+// Delete a post by id
+async function deleteEepost(id) {
+    return await Eepost.deleteOne({_id : id})
+}
+
+// Fix the content of a post
+async function updateEepost(id, content) {
+    return await Eepost.findByIdAndUpdate(id, {$set : {content : content}})
+}
+
+// Add a like to a post
+async function like(id) {
+    return await Eepost.findByIdAndUpdate(id, {$inc : {like : 1}})
+}
+
 export {
     retrieveAllEepostList,
+    addEepost,
     retrieveEepostListByEntryId,
+    deleteEepost,
+    updateEepost,
+    like
 }
